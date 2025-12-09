48ffeb58a0f69f6ab5983e283182b06f76d0b8f2
VTMatheusVT <vttheus@gmail.com>
2025-12-09 00:46:17 -0300

    Refactor fila_espera.h for consistency and formatting

--- include/fila_espera.h
+++ include/fila_espera.h
@@ -1,26 +1,23 @@
 #ifndef FILA_ESPERA_H
 #define FILA_ESPERA_H
 
-#include "hospede_lista.h" // Inclui a definição de HOSPEDE
+#include "hospede_lista.h"
 
-// Estrutura do nó da Fila
 typedef struct no_fila {
-    HOSPEDE elem; // Armazena o hóspede em espera
+    HOSPEDE elem;
     struct no_fila *prox;
 } NO_FILA;
 
-// Estrutura da Fila
 typedef struct {
     NO_FILA *inicio;
     NO_FILA *fim;
     int tamanho;
 } FILA_ESPERA;
 
-// Protótipos das funções da Fila
 void cria_fila_vazia(FILA_ESPERA *f);
 int fila_vazia(FILA_ESPERA f);
-void Enfileira(FILA_ESPERA *f, HOSPEDE x); // Enqueue (Entrada na lista de espera)
-HOSPEDE Desenfileira(FILA_ESPERA *f); // Dequeue (Atendimento do próximo)
+void Enfileira(FILA_ESPERA *f, HOSPEDE x);
+HOSPEDE Desenfileira(FILA_ESPERA *f);
 void exibe_fila(FILA_ESPERA f);
 
 #endif
