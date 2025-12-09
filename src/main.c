ac94b4bc0a19c09157263c9653ec9f9fb69fe5dc
VTMatheusVT <vttheus@gmail.com>
2025-12-09 00:48:16 -0300

    Refactor input handling and menu display

--- src/main.c
+++ src/main.c
@@ -14,7 +14,8 @@ int main() {
     HOSPEDE X;
     RESERVA R;
 
-    int opcao, cod, prox_id_reserva = 1; // Contador simples para ID de Reserva
+    int opcao, cod, prox_id_reserva = 1;
+    char temp_char[50];
 
     criar_hotel_vazio(&H);
     criar_lista_reserva_vazia(&LR);
@@ -23,7 +24,11 @@ int main() {
 
     do {
         MENU();
-        scanf("%d", &opcao);
+        if (scanf("%d", &opcao) != 1) {
+            fgets(temp_char, sizeof(temp_char), stdin);
+            opcao = -1; 
+        }
+        getchar();
 
         switch (opcao) {
             case 1: // RF01: Cadastrar Hospede
@@ -34,13 +39,7 @@ int main() {
             case 2: // RF03: Cadastrar Reserva
                 printf("Entre com o ID do hospede para a reserva: ");
                 scanf("%d", &cod);
-                
-                // Simulação: se o hóspede existe, permite a reserva
-                // (Em um sistema real, haveria busca e verificação de quarto livre)
-                
-                // A função ler_reserva precisaria de um Hóspede real.
-                // Aqui, assumimos que o ID foi digitado corretamente para fins de exemplo
-                // Para uma implementação completa, seria necessária uma busca pelo HOSPEDE.
+                getchar();
                 
                 ler_reserva(&R, prox_id_reserva, cod);
                 Insere_reserva(&LR, R);
@@ -48,19 +47,21 @@ int main() {
                 break;
 
             case 3: // RF05: Realizar Check-in (Simplificado)
-                printf("Entre com o ID do hospede para o Check-in (Este hospede deve ter uma Reserva): ");
+                printf("Entre com o ID do hospede para o Check-in: ");
                 scanf("%d", &cod);
-                printf("Simulando Check-in para o Hospede ID %d. Quarto marcado como ocupado.\n", cod);
-                // Lógica complexa de busca na reserva e marcação do quarto no sistema real.
+                getchar();
+                printf("Simulando Check-in para o Hospede ID %d. Quarto marcado como ocupado (RF05).\n", cod);
                 break;
             
             case 4: // RF06: Realizar Check-out
                 printf("Entre com o ID do hospede para o Check-out: ");
                 scanf("%d", &cod);
-                Remover_hospede(&H, cod, &X); // Remove da lista de Hóspedes Ativos
+                getchar();
                 
-                if (X.id != 0) { // Se o hóspede foi removido com sucesso
-                    Empilha(&PC, X); // Registra no Histórico de Check-outs (Pilha)
+                Remover_hospede(&H, cod, &X);
+                
+                if (X.id != 0) {
+                    Empilha(&PC, X);
                 }
                 break;
                 
@@ -72,35 +73,43 @@ int main() {
                 Exibe_Todas_Reservas(LR);
                 break;
                 
-            case 7: // Manipulação da Fila de Espera
-                printf("\n--- Opcoes da Fila de Espera ---\n");
-                printf("1 - Adicionar Hospede na Espera\n");
-                printf("2 - Atender Proximo da Espera\n");
+            case 7: // Gerenciar Fila de Espera
+                printf("\n--- Opcoes da Fila de Espera (Fila) ---\n");
+                printf("1 - Adicionar Hospede na Espera (Enfileirar)\n");
+                printf("2 - Atender Proximo da Espera (Desenfileirar)\n");
                 printf("3 - Exibir Fila\n");
                 printf("Opcao: ");
                 int sub_opcao_fila;
-                scanf("%d", &sub_opcao_fila);
+                if (scanf("%d", &sub_opcao_fila) != 1) {
+                    fgets(temp_char, sizeof(temp_char), stdin); 
+                    break;
+                }
+                getchar();
 
                 if (sub_opcao_fila == 1) {
-                    ler_hospede(&X); // Lê os dados do hospede para a fila
+                    ler_hospede(&X);
                     Enfileira(&FE, X);
                 } else if (sub_opcao_fila == 2) {
                     X = Desenfileira(&FE);
                     if (X.id != 0) {
-                        printf("Hospede %s pronto para check-in.\n", X.nome);
+                        printf("Hospede %s pronto para Check-in.\n", X.nome);
                     }
                 } else if (sub_opcao_fila == 3) {
                     exibe_fila(FE);
                 }
                 break;
                 
-            case 8: // Manipulação da Pilha de Check-outs
+            case 8: // Gerenciar Pilha de Check-outs
                 printf("\n--- Opcoes da Pilha (Historico de Check-outs) ---\n");
-                printf("1 - Revisar Ultimo Check-out (Desempilhar)\n");
+                printf("1 - Revisar Ultimo Check-out (Desempilhar - LIFO)\n");
                 printf("2 - Exibir Historico\n");
                 printf("Opcao: ");
                 int sub_opcao_pilha;
-                scanf("%d", &sub_opcao_pilha);
+                if (scanf("%d", &sub_opcao_pilha) != 1) {
+                     fgets(temp_char, sizeof(temp_char), stdin); 
+                     break;
+                }
+                getchar();
                 
                 if (sub_opcao_pilha == 1) {
                     X = Desempilha(&PC);
@@ -113,7 +122,7 @@ int main() {
                 break;
 
             case 9:
-                printf("Obrigado por escolher nosso produto\n");
+                printf("Encerrando o sistema. Obrigado!\n");
                 break;
 
             default:
@@ -129,14 +138,14 @@ void MENU() {
     printf("\n===================================\n");
     printf("         SISTEMA DE HOTELARIA      \n");
     printf("===================================\n");
-    printf("1 - Cadastrar novo hospede (RF01)\n");
-    printf("2 - Cadastrar nova reserva (RF03)\n");
+    printf("1 - Cadastrar novo hospede (Lista H) (RF01)\n");
+    printf("2 - Cadastrar nova reserva (Lista R) (RF03)\n");
     printf("3 - Realizar Check-in (RF05)\n");
-    printf("4 - Realizar Check-out (RF06, Pilha)\n");
+    printf("4 - Realizar Check-out (Remove de Lista H e Empilha em Pilha) (RF06)\n");
     printf("-----------------------------------\n");
     printf("5 - Listar todos os Hospedes (RF02)\n");
     printf("6 - Listar todas as Reservas Ativas (RF04)\n");
-    printf("7 - Gerenciar Fila de Espera (Fila)\n");
+    printf("7 - Gerenciar Fila de Espera\n");
     printf("8 - Gerenciar Historico de Check-outs (Pilha)\n");
     printf("9 - Encerrar o Trabalho\n");
     printf("Opcao: ");
