3ba09cd9cb7e00ebbf40036b2a290aadb4b7d140
VTMatheusVT <vttheus@gmail.com>
2025-12-09 00:47:00 -0300

    Fix formatting and include guards in pilha_checkout.h

--- include/pilha_checkout.h
+++ include/pilha_checkout.h
@@ -1,25 +1,22 @@
 #ifndef PILHA_CHECKOUT_H
 #define PILHA_CHECKOUT_H
 
-#include "hospede_lista.h" // Inclui a definição de HOSPEDE
+#include "hospede_lista.h"
 
-// Estrutura do nó da Pilha
 typedef struct no_pilha {
-    HOSPEDE elem; // Armazena o hóspede que fez check-out
+    HOSPEDE elem;
     struct no_pilha *prox;
 } NO_PILHA;
 
-// Estrutura da Pilha
 typedef struct {
     NO_PILHA *topo;
     int tamanho;
 } PILHA_CHECKOUT;
 
-// Protótipos das funções da Pilha
 void cria_pilha_vazia(PILHA_CHECKOUT *p);
 int pilha_vazia(PILHA_CHECKOUT p);
-void Empilha(PILHA_CHECKOUT *p, HOSPEDE x); // Push (Registra Check-out)
-HOSPEDE Desempilha(PILHA_CHECKOUT *p); // Pop (Revisa/Desfaz último Check-out)
+void Empilha(PILHA_CHECKOUT *p, HOSPEDE x);
+HOSPEDE Desempilha(PILHA_CHECKOUT *p);
 void exibe_pilha(PILHA_CHECKOUT p);
 
 #endif
